cb3757c442b02f22347babb86c8500e71d1572fa
unknown <tgalcik1@umbc.edu>
2023-10-14 19:16:01 -0400

    all selected countries remain highlighted, max of 2. removed mouseover effects temporarily

--- forex-vis/src/MapComponent.js
+++ forex-vis/src/MapComponent.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import * as d3 from 'd3';
-import './MapComponent.css';
 import countriesGeoJSON from './data/countries.json';
+import './MapComponent.css';
+import * as d3 from 'd3';
 
-function MapComponent() {
-  const center = [35, 25]; // initial center coordinates
-  const [showModal, setShowModal] = React.useState(false); //sets modal off as default
-  const [selectedCountries, setSelectedCountries] = React.useState([]); // Selected countries
+const MapComponent = () => {
+  const [selectedCountries, setSelectedCountries] = useState([]);
+  
+  const geoJSONStyle = (feature) => {
+    const countryName = feature.properties.NAME;
+    const isSelected = selectedCountries.includes(countryName);
+    
+    return {
+      fillColor: isSelected ? 'darkred' : 'green',
+      weight: 1,
+      opacity: 1,
+      color: isSelected ? 'white' : 'white',
+      dashArray: isSelected ? '' : '3',
+      fillOpacity: isSelected ? 0.6 : 0.2,
+    };
+  };
+  
+  const toggleCountrySelection = (countryName) => {
+    setSelectedCountries((prevSelectedCountries) => {
+      if (prevSelectedCountries.includes(countryName)) {
+        // if the country is already selected, unselect it
+        return prevSelectedCountries.filter((name) => name !== countryName);
+      } else if (prevSelectedCountries.length < 2) {
+        // if the country is not selected and you have less than 2 selections, add it
+        return [...prevSelectedCountries, countryName];
+      } else {
+        // if you have reached the limit of 2 selections, replace the least recently selected one
+        return [prevSelectedCountries[1], countryName];
+      }
+    });
+  };
 
-  React.useEffect(() => {
-    if (selectedCountries.length > 2){
-      toggleModal();
-      setSelectedCountries(selectedCountries.slice(2));
-    }
-    if (selectedCountries.length === 2) {
-      toggleModal();
-    }
-  }, [selectedCountries]);
-
+  // temporary stuff for data
   const data = [1, 2, 3, 4, 5];
   const width = 640;
   const height = 400;
@@ -32,128 +50,15 @@ function MapComponent() {
   const y = d3.scaleLinear().domain(d3.extent(data)).range([height - marginBottom, marginTop]);
   const line = d3.line().x((d, i) => x(i)).y(y);
 
-  const geoJSONStyle = (feature) => {
-    return {
-      fillColor: 'green',
-      weight: 1,
-      opacity: 1,
-      color: 'white',
-      dashArray: '3',
-      fillOpacity: 0.2,
-    };
-  };
-
-  const maxBounds = [
-    [-180, -180],
-    [180, 347],
-  ];
-
-  //Toggles the modal on and off
-  const toggleModal = () => {
-    setShowModal((current) => !current);
-  };
-
-  const mouseDownFeature = (e) => {
-    const layer = e.target;
-    const countryName = e.sourceTarget.feature.properties.NAME;
-    
-    setSelectedCountries((prevSelectedCountries) => {
-      // Check if the countryName is not already in the selectedCountries array
-      if (!prevSelectedCountries.includes(countryName)) {
-        return [...prevSelectedCountries, countryName];
-      }else {
-        return prevSelectedCountries;
-      }
-    });
-  };
-
-  const mouseUpFeature = (e) => {
-    const layer = e.target;
-    layer.setStyle({
-      weight: 1.5,
-      color: '047127',
-      dashArray: '',
-      fillOpacity: 0.6,
-    });
-
-    layer.bringToFront();
-  };
-
-  const highlightFeature = (e) => {
-    const layer = e.target;
-    const countryName = e.sourceTarget.feature.properties.NAME;
-
-    setSelectedCountries((prevSelectedCountries) => {
-      if (!prevSelectedCountries.includes(countryName)) {
-        layer.setStyle({
-          weight: 1.5,
-          color: 'white',
-          dashArray: '',
-          fillOpacity: 0.4,
-        });
-    
-        layer.bringToFront();
-      }
-      return prevSelectedCountries;
-    });
-  };
-
-  const resetHighlight = (e) => {
-    const layer = e.target;
-    const countryName = e.sourceTarget.feature.properties.NAME;
-
-    setSelectedCountries((prevSelectedCountries) => {
-      if (!prevSelectedCountries.includes(countryName)) {
-        layer.setStyle({
-          weight: 1,
-          color: 'white',
-          dashArray: '3',
-          fillOpacity: 0.2,
-        });
-      }
-      return prevSelectedCountries;
-    });
-  };
-  
-
   return (
-    <MapContainer
-      center={center}
-      zoom={3}
-      style={{ width: '100vw', height: '100vh' }}
-      maxBounds={maxBounds}
-      maxBoundsViscosity={1.0}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        minZoom={3}
-        maxZoom={4}
-      />
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+          <div className='overlay-div'>
+            <h1>Foreign Exchange Visualization</h1>
 
-      <GeoJSON
-        data={countriesGeoJSON}
-        style={geoJSONStyle}
-        onEachFeature={(feature, layer) => {
-          layer.on({
-            mousedown: mouseDownFeature,
-            mouseup: mouseUpFeature,
-            mouseover: highlightFeature,
-            mouseout: resetHighlight,
-          });
-        }}
-      />
-
-      {/* This section should be populared with our dataset. Should probably move to separate file... */}
-      {showModal && 
-        <div className="overlay-div">
-          
-          <h1>Foreign Exchange Visualization</h1>
-
-          <h2>Showing forex data for:</h2>
-          {selectedCountries.slice(0,2).map((country, index) => (
-            <li key={index}>{country}</li>
-          ))}
+            <h2>Showing forex data for:</h2>
+            {selectedCountries.slice(0,2).map((country, index) => (
+              <li key={index}>{country}</li>
+            ))}
 
           <p>this is where we can generate graphs</p>
           <div className="forexChart">
@@ -166,12 +71,29 @@ function MapComponent() {
               </g>
             </svg>
           </div>
-        
-        </div>
-      }
-      
-    </MapContainer>
+
+          </div>
+          <MapContainer center={[35, 25]} zoom={3} style={{ width: '100%', height: '100%', zIndex: 0 }}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              minZoom={3}
+              maxZoom={4}
+            />
+
+            <GeoJSON
+              data={countriesGeoJSON}
+              style={geoJSONStyle}
+              onEachFeature={(feature, layer) => {
+                const countryName = feature.properties.NAME;
+                layer.on({
+                  click: () => toggleCountrySelection(countryName),
+                });
+              }}
+            />
+          </MapContainer>
+      </div>
   );
-}
+};
 
 export default MapComponent;
