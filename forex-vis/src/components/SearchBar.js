9b27b05a1f34f1ef69bc71ee97fb974b38c84257
Rostam B. Rad <rostamrad@Rostams-MacBook-Pro.local>
2023-10-26 17:03:12 -0400

    Made changes to search functionality

--- forex-vis/src/components/SearchBar.js
+++ forex-vis/src/components/SearchBar.js
@@ -4,19 +4,29 @@ import './SearchBar.css';
 const SearchBar = (props) => {
   const [searchValue, setSearchValue] = useState('');
   console.log(props);
-  const handleSearch = () => {
-    props.toggleCountrySelection(searchValue)
-    console.log(searchValue); 
 
+  const handleSearch = () => {
+    props.toggleCountrySelection(searchValue);
+    console.log(searchValue);
+    setSearchValue(''); // Clear the input box after searching
+  };
+
+  const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
   };
 
   const handleInputChange = (event) => {
-    setSearchValue(event.target.value);
+    const inputValue = event.target.value;
+    const capitalizedInput = inputValue
+      .split(' ')
+      .map(capitalizeFirstLetter)
+      .join(' ');
+    setSearchValue(capitalizedInput);
   };
 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
-      handleSearch(); 
+      handleSearch();
     }
   };
 
